19546a53004834019a30023b7d64ae2756dda08d
samned <samuelabebayehu@gmail.com>
2023-02-09 23:44:47 +0300

    updated device categories

--- modern/src/map/core/preloadImages.js
+++ modern/src/map/core/preloadImages.js
@@ -1,50 +1,52 @@
 import palette from '../../common/theme/palette';
 import { loadImage, prepareIcon } from './mapUtil';
 
+import defaultSvg from '../../resources/images/icon/default.svg';
 import directionSvg from '../../resources/images/direction.svg';
 import backgroundSvg from '../../resources/images/background.svg';
-import animalSvg from '../../resources/images/icon/animal.svg';
-import bicycleSvg from '../../resources/images/icon/bicycle.svg';
 import boatSvg from '../../resources/images/icon/boat.svg';
 import busSvg from '../../resources/images/icon/bus.svg';
 import carSvg from '../../resources/images/icon/car.svg';
 import craneSvg from '../../resources/images/icon/crane.svg';
-import defaultSvg from '../../resources/images/icon/default.svg';
-import helicopterSvg from '../../resources/images/icon/helicopter.svg';
 import motorcycleSvg from '../../resources/images/icon/motorcycle.svg';
-import offroadSvg from '../../resources/images/icon/offroad.svg';
 import personSvg from '../../resources/images/icon/person.svg';
 import pickupSvg from '../../resources/images/icon/pickup.svg';
-import planeSvg from '../../resources/images/icon/plane.svg';
-import scooterSvg from '../../resources/images/icon/scooter.svg';
-import shipSvg from '../../resources/images/icon/ship.svg';
 import tractorSvg from '../../resources/images/icon/tractor.svg';
 import trainSvg from '../../resources/images/icon/train.svg';
-import tramSvg from '../../resources/images/icon/tram.svg';
-import trolleybusSvg from '../../resources/images/icon/trolleybus.svg';
 import truckSvg from '../../resources/images/icon/truck.svg';
 import vanSvg from '../../resources/images/icon/van.svg';
+import boomliftSvg from '../../resources/images/icon/boomlift.svg';
+import catSvg from '../../resources/images/icon/cat.svg';
+import dogSvg from '../../resources/images/icon/dog.svg';
+import droneSvg from '../../resources/images/icon/drone.svg';
+import firetruckSvg from '../../resources/images/icon/firetruck.svg';
+import foodtruckSvg from '../../resources/images/icon/foodtruck.svg';
+import schoolvanSvg from '../../resources/images/icon/schoolvan.svg';
+import securitycarSvg from '../../resources/images/icon/securitycar.svg';
+import skyjackSvg from '../../resources/images/icon/skyjack.svg';
+import suvSvg from '../../resources/images/icon/suv.svg';
 
 export const mapIcons = {
-  animal: animalSvg,
-  bicycle: bicycleSvg,
+  default: defaultSvg,
   boat: boatSvg,
+  boomlift: boomliftSvg,
   bus: busSvg,
   car: carSvg,
+  cat: catSvg,
   crane: craneSvg,
-  default: defaultSvg,
-  helicopter: helicopterSvg,
+  dog: dogSvg,
+  drone: droneSvg,
+  firetruck: firetruckSvg,
+  foodtruck: foodtruckSvg,
   motorcycle: motorcycleSvg,
-  offroad: offroadSvg,
   person: personSvg,
   pickup: pickupSvg,
-  plane: planeSvg,
-  scooter: scooterSvg,
-  ship: shipSvg,
+  schoolvan: schoolvanSvg,
+  securitycar: securitycarSvg,
+  skyjack: skyjackSvg,
+  suv: suvSvg,
   tractor: tractorSvg,
   train: trainSvg,
-  tram: tramSvg,
-  trolleybus: trolleybusSvg,
   truck: truckSvg,
   van: vanSvg,
 };
